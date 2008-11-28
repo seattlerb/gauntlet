1bb35dd81eb974ac54d3beb9f4e4c3ee98c5f6e0
Ryan Davis <ryand@zenspider.com>
2008-11-28 15:30:18 -0800

    Added -F to make a fast update mode. Cleaned up the code from a module to a class. Started getting the API right for proper subclassing nad customization. Took a stab at threading it, but I think rubygems has a bug in their fetcher.
    [git-p4: depot-paths = "//src/gauntlet/dev/": change = 4481]

--- lib/gauntlet.rb
+++ lib/gauntlet.rb
@@ -1,76 +1,82 @@
 require 'rubygems/remote_fetcher'
+require 'thread'
 
 $u ||= false
+$f ||= false
+$F ||= false
 
-module GemUpdater
+Thread.abort_on_exception = true
+
+class Gauntlet
+  VERSION = '1.0.0'
+
   GEMURL = URI.parse 'http://gems.rubyforge.org'
+  GEMDIR = File.expand_path "~/.gauntlet"
 
-  @@index = nil
+  # stupid dups usually because of "dash" renames
+  # TODO: probably stale by now - try to generate dynamically
+  STUPID_GEMS = %w(ruby-aes-table1-1.0.gem
+                   ruby-aes-unroll1-1.0.gem
+                   hpricot-scrub-0.2.0.gem
+                   extract_curves-0.0.1.gem
+                   extract_curves-0.0.1-i586-linux.gem
+                   extract_curves-0.0.1-mswin32.gem
+                   rfeedparser-ictv-0.9.931.gem
+                   spec_unit-0.0.1.gem)
 
-  def self.stupid_gems
-    ["ruby-aes-table1-1.0.gem", # stupid dups usually because of "dash" renames
-     "ruby-aes-unroll1-1.0.gem",
-     "hpricot-scrub-0.2.0.gem",
-     "extract_curves-0.0.1.gem",
-     "extract_curves-0.0.1-i586-linux.gem",
-     "extract_curves-0.0.1-mswin32.gem",
-     "rfeedparser-ictv-0.9.931.gem",
-     "spec_unit-0.0.1.gem"]
-  end
-
-  def self.initialize_dir
-    Dir.mkdir "../gems" unless File.directory? "../gems"
-    self.in_gem_dir do
+  def initialize_dir
+    Dir.mkdir GEMDIR unless File.directory? GEMDIR
+    in_gem_dir do
       File.symlink ".", "cache" unless File.exist? "cache"
     end
   end
 
-  def self.get_source_index
-    return @@index if @@index
+  def get_source_index
+    @index ||= in_gem_dir do
+      dump = if ($u and not $F) or not File.exist? '.source_index' then
+               url = GEMURL + "Marshal.#{Gem.marshal_version}.Z"
+               dump = Gem::RemoteFetcher.fetcher.fetch_path url
+               require 'zlib'
+               dump = Gem.inflate dump
+               open '.source_index', 'wb' do |io| io.write dump end
+               dump
+             else
+               open '.source_index', 'rb' do |io| io.read end
+             end
 
-    dump = if $u or not File.exist? '.source_index' then
-             url = GEMURL + "Marshal.#{Gem.marshal_version}.Z"
-             dump = Gem::RemoteFetcher.fetcher.fetch_path url
-             require 'zlib'
-             dump = Gem.inflate dump
-             open '.source_index', 'wb' do |io| io.write dump end
-             dump
-           else
-             open '.source_index', 'rb' do |io| io.read end
-           end
-
-    @@index = Marshal.load dump
+      Marshal.load dump
+    end
   end
 
-  def self.get_latest_gems
-    @@cache ||= get_source_index.latest_specs
+  def get_latest_gems
+    @cache ||= get_source_index.latest_specs
   end
 
-  def self.get_gems_by_name
-    @@by_name ||= Hash[*get_latest_gems.map { |gem|
-                         [gem.name, gem, gem.full_name, gem]
-                       }.flatten]
+  def get_gems_by_name
+    @by_name ||= Hash[*get_latest_gems.map { |gem|
+                        [gem.name, gem, gem.full_name, gem]
+                      }.flatten]
   end
 
-  def self.dependencies_of name
-    index = self.get_source_index
+  def dependencies_of name
+    index = get_source_index
     get_gems_by_name[name].dependencies.map { |dep| index.search(dep).last }
   end
 
-  def self.dependent_upon name
+  def dependent_upon name
     get_latest_gems.find_all { |gem|
       gem.dependencies.any? { |dep| dep.name == name }
     }
   end
 
-  def self.update_gem_tarballs
-    GemUpdater.initialize_dir
+  def update_gem_tarballs
+    initialize_dir
 
-    latest = GemUpdater.get_latest_gems
+    latest = get_latest_gems
 
     puts "updating mirror"
 
-    self.in_gem_dir do
+    in_gem_dir do
       gems = Dir["*.gem"]
       tgzs = Dir["*.tgz"]
 
@@ -82,44 +88,58 @@ module GemUpdater
         end
       end
 
-      new = latest.map { |spec|
-        "#{spec.full_name}.tgz"
-      } - tgzs
+      tasks = Queue.new
+      latest.sort!
+      latest.reject! { |spec| tgzs.include? "#{spec.full_name}.tgz" }
+      tasks.push(latest.shift) until latest.empty? # LAME
 
-      puts "fetching #{new.size} tgzs"
+      puts "fetching #{tasks.size} gems"
 
-      latest.sort.each do |spec|
-        full_name = spec.full_name
-        tgz_name = "#{full_name}.tgz"
-        gem_name = "#{full_name}.gem"
+      threads = []
+      1.times do
+        threads << Thread.new do
+          until tasks.empty? do
+            spec = tasks.shift
+            full_name = spec.full_name
+            tgz_name = "#{full_name}.tgz"
+            gem_name = "#{full_name}.gem"
 
-        next if tgzs.include? tgz_name
+            unless gems.include? gem_name then
+              begin
+                warn "downloading #{full_name}"
+                Gem::RemoteFetcher.fetcher.download(spec, GEMURL, Dir.pwd)
+              rescue Gem::RemoteFetcher::FetchError
+                warn "  failed"
+                next
+              end
+            end
 
-        unless gems.include? gem_name then
-          begin
-            warn "downloading #{full_name}"
-            Gem::RemoteFetcher.fetcher.download(spec, GEMURL, Dir.pwd)
-          rescue Gem::RemoteFetcher::FetchError
-            warn "  failed"
-            next
+            warn "  converting #{gem_name} to tarball"
+
+            unless File.directory? full_name then
+              system "gem unpack cache/#{gem_name} &> /dev/null"
+              system "gem spec -l cache/#{gem_name} > #{full_name}/gemspec.rb"
+            end
+
+            system "chmod -R u+w #{full_name}"
+            system "tar zmcf #{tgz_name} #{full_name}"
+            system "rm -rf   #{full_name} #{gem_name}"
           end
         end
+      end
 
-        warn "converting #{gem_name} to tarball"
-
-        unless File.directory? full_name then
-          system "gem unpack cache/#{gem_name}"
-          system "gem spec -l cache/#{gem_name} > #{full_name}/gemspec.rb"
-        end
-
-        system "tar zmcf #{tgz_name} #{full_name}"
-        system "rm -rf   #{full_name} #{gem_name}"
+      threads.each do |thread|
+        thread.join
       end
     end
+  rescue Interrupt
+    warn "user cancelled... quitting"
+    exit 1
   end
 
-  def self.each_gem filter = /^[\w-]+-\d+(\.\d+)*\.tgz$/
-    self.in_gem_dir do
+  def each_gem filter = nil
+    filter ||= /^[\w-]+-\d+(\.\d+)*\.tgz$/
+    in_gem_dir do
       Dir["*.tgz"].each do |tgz|
         next unless tgz =~ filter
 
@@ -128,8 +148,8 @@ module GemUpdater
     end
   end
 
-  def self.with_gem name
-    self.in_gem_dir do
+  def with_gem name
+    in_gem_dir do
       begin
         system "tar zxmf #{name}.tgz 2> /dev/null"
         Dir.chdir name do
@@ -141,11 +161,11 @@ module GemUpdater
     end
   end
 
-  def self.load_yaml path, default = {}
+  def load_yaml path, default = {}
     YAML.load(File.read(path)) rescue default
   end
 
-  def self.save_yaml path, data
+  def save_yaml path, data
     File.open("#{path}.new", 'w') do |f|
       warn "*** saving #{path}"
       YAML.dump data, f
@@ -153,9 +173,28 @@ module GemUpdater
     File.rename "#{path}.new", path
   end
 
-  def self.in_gem_dir
-    Dir.chdir "../gems" do
+  def in_gem_dir
+    Dir.chdir GEMDIR do
       yield
+    end
+  end
+
+  def run name
+    raise "subclass responsibility"
+  end
+
+  def run_the_gauntlet filter = nil
+    initialize_dir
+    update_gem_tarballs if $u
+
+    each_gem filter do |name|
+      with_gem name do
+        if block_given? then
+          yield name
+        else
+          run name
+        end
+      end
     end
   end
 end
